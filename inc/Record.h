5dacada49df2e93d6def71cf75d0a99def0c81df
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-25 18:09:25 +0200

    Update translation unit Record

--- inc/Record.h
+++ inc/Record.h
@@ -1,26 +1,42 @@
 
 // Record.h
-
+// ----------------------------------------------------------------
 #ifndef RECORD_H
 #define RECORD_H
 
-#include <string>
-
 using namespace std;
-
+// ----------------------------------------------------------------
 class Record {
 
     private:
 
-        string _dateTime;
-        double _temperature;
+        int    _year;
+        int    _month;
+        int    _day;
+        double _temp;
+        double _tempUrban;
+        int    _dataId;
 
     public:
 
-        Record(const string dateTime, const double temperature);
-        string dateTime() const { return _dateTime; }
-        double temperature() const { return _temperature; }
+        Record(
+            const int    year,
+            const int    month,
+            const int    day,
+            const double temp,
+            const double tempUrban,
+            const int    dataId
+        );
+
+        int    year()      const {return _year;}
+        int    month()     const {return _month;}
+        int    day()       const {return _day;}
+        double temp()      const {return _temp;}
+        double tempUrban() const {return _tempUrban;}
+        int    dataId()    const {return _dataId;}
+
         void print() const;
 };
-
+// ----------------------------------------------------------------
 #endif // RECORD_H
+// ----------------------------------------------------------------
---
+++ src/Record.cpp
@@ -0,0 +1,35 @@
+
+// Record.cxx
+// ----------------------------------------------------------------
+#include <iostream>
+
+#include "Record.h"
+
+using namespace std;
+// ----------------------------------------------------------------
+Record::Record(
+            const int    year,
+            const int    month,
+            const int    day,
+            const double temp,
+            const double tempUrban,
+            const int    dataId
+        )
+{
+        _year      = year;
+        _month     = month;
+        _day       = day;
+        _temp      = temp;
+        _tempUrban = tempUrban;
+        _dataId    = dataId;
+}
+// ----------------------------------------------------------------
+void Record::print() const {
+    cout << year()      << "\t"
+         << month()     << "\t"
+         << day()       << "\t"
+         << temp()      << "°C\t" 
+         << tempUrban() << "°C\t" 
+         << dataId()    << endl;
+}
+// ----------------------------------------------------------------
