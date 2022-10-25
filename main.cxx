ea7de6914bcef5bede634c6bd36fb547fc684b75
Pinar Oncel <90771292+pinaroncel@users.noreply.github.com>
2022-10-25 09:22:48 +0200

    build program flow
    consists of a simple for loop that will run once for each input file

--- main.cxx
+++ main.cxx
@@ -1,3 +1,34 @@
 
 // main.cxx
+// ----------------------------------------------------------------
+#include <iostream>
+#include <string>
+#include <vector>
+// ----------------------------------------------------------------
+int main() {
+  
+    vector<string> files = {"file1.dat", "file2.dat", "file3.dat"};
+    
+    for (auto file : files) {
+  
+        cout << " ... starting projectNPT ... " << endl;
 
+        // Read data file a single file
+
+        // Analyze data that has been read in -> Model 1
+
+        // Plot data that has been analyzed -> Model 1
+
+        // Analyze data that has been read in -> Model 2
+
+        // Plot data that has been analyzed -> Model 2
+
+        // Analyze data that has been read in -> Model 3
+
+        // Plot data that has been analyzed -> Model 3
+
+        cout << " ... ending projectNPT ... " endl;
+
+    return 0;
+}
+// ----------------------------------------------------------------
