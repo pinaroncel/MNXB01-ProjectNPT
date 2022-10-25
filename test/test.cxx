d18dd0c27ad421b45df79cacf5b7c49372a72d75
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-25 15:27:09 +0200

    Test dateTime

---
+++ test/test.cxx
@@ -0,0 +1,19 @@
+
+// test.cxx
+
+#include <chrono>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+
+    auto x = std::time();
+
+    cout << x << endl;
+
+    return 0;
+
+}
