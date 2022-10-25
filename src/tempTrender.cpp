aede4d03e327678c8057bad1ed4f68e8f57eef93
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-25 18:20:09 +0200

    uodate translation unit tempTrender

--- src/tempTrender.cpp
+++ src/tempTrender.cpp
@@ -1,13 +1,28 @@
+
+// tempTrender.cpp
+// ----------------------------------------------------------------
 #include <iostream>
+
 #include "tempTrender.h"
 
-tempTrender::tempTrender(const std::string& filePath) {
-	std::cout << "The user supplied " << filePath <<" as the path to the data file.\n";
-	std::cout << "You should probably store this information in a member variable of the class! Good luck with the project! :)\n";
+using namespace std;
+// ----------------------------------------------------------------
+tempTrender::tempTrender(const string& filePath) {
+	cout << "The user supplied " << filePath <<" as the path to the data file." << endl;
+	cout << "You should probably store this information in a member variable of the class! Good luck with the project! :)" << endl;
 }
 
-// void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const {} //Make a histogram of the temperature on this day
-// void tempTrender::tempOnDay(int dateToCalculate) const {} //Make a histogram of the temperature on this date
-// void tempTrender::tempPerDay() const {} //Make a histogram of the average temperature of each day of the year
-// void tempTrender::hotCold() const {} //Make a histogram of the hottest and coldest day of the year
-// void tempTrender::tempPerYear(int yearToExtrapolate) const {} //Make a histogram of average temperature per year, then fit and extrapolate to the given year
+// Make a histogram of the temperature on this day
+// void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const {}
+
+// Make a histogram of the temperature on this date
+// void tempTrender::tempOnDay(int dateToCalculate) const {}
+
+// Make a histogram of the average temperature of each day of the year
+// void tempTrender::tempPerDay() const {}
+
+// Make a histogram of the hottest and coldest day of the year
+// void tempTrender::hotCold() const {}
+
+// Make a histogram of average temperature per year, then fit and extrapolate to the given year
+// void tempTrender::tempPerYear(int yearToExtrapolate) const {}
