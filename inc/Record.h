a16b980737751cac32bc38233812527ff0ad6054
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-25 15:26:44 +0200

    Add translation unit Record

--- inc/Record.h
+++ inc/Record.h
@@ -1,5 +1,26 @@
 
 // Record.h
 
-// Class definition for Record
+#ifndef RECORD_H
+#define RECORD_H
 
+#include <string>
+
+using namespace std;
+
+class Record {
+
+    private:
+
+        string _dateTime;
+        double _temperature;
+
+    public:
+
+        Record(const string dateTime, const double temperature);
+        string dateTime() const { return _dateTime; }
+        double temperature() const { return _temperature; }
+        void print() const;
+};
+
+#endif // RECORD_H
--- src/Record.cxx
+++ src/Record.cxx
@@ -1,5 +1,18 @@
 
 // Record.cxx
 
-// Class implementation for Record
+#include <iostream>
+#include <string>
 
+#include "Record.h"
+
+using namespace std;
+
+Record::Record(string dateTime, double temperature) {
+    _dateTime = dateTime;
+    _temperature = temperature;
+}
+
+void Record::print() const {
+    cout << "Date(" << Record::dateTime() << ") Temperature(" << Record::temperature() << " °C)" << endl;
+}
