e1625dd15fd7f9f3322e1fc53cef4adeb26b82e8
tildebw <113617822+tildebw@users.noreply.github.com>
2022-10-26 21:23:04 +0200

    Update Plot.h

--- include/Plot.h
+++ include/Plot.h
@@ -1,4 +1,6 @@
 
-void make_graph(const vector<double>& years, const vector<double>& temps);
+void make_graph_av(const vector<double>& years, const vector<double>& temps);
+
+void make_graph_day(const vector<double>& years, const vector<double>& temps);
 
 void make_graph_min_max(const vector<double>& years, const vector<double>& minTemps, const vector<double>& maxTemps, const vector<double>& diffTemps);
