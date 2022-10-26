9eab908af87fc2c4b2862795c27615968f36b73a
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-26 17:58:33 +0200

    Nikolei adds Read translation unit

---
+++ src/Read.cpp
@@ -0,0 +1,24 @@
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Record.h"
+
+vector<Record> readFromFile(string filename) {
+    vector<Record> records;
+    ifstream input(filename);
+    string line;
+    while(getline(input, line)) {
+        int    year      = stoi(line.substr(0, 4));
+        int    month     = stoi(line.substr(5, 3));
+        int    day       = stoi(line.substr(8, 3));
+        double temp      = stod(line.substr(11, 6));
+        double tempUrban = stod(line.substr(17, 6));
+        int    dataId    = stoi(line.substr(23, 2));
+        Record record = Record(year, month, day, temp, tempUrban, dataId);
+        records.push_back(record);
+    }
+    return records;
+}
