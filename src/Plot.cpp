173699b37dd336bb896a137f1a08bc36070ce295
tildebw <113617822+tildebw@users.noreply.github.com>
2022-10-26 21:22:11 +0200

    Update Plot.cpp

--- src/Plot.cpp
+++ src/Plot.cpp
@@ -5,7 +5,6 @@
 
 #include "Record.h"
 
-#include <TCanvas.h>
 #include <TPad.h>
 #include <TFrame.h>
 #include <TStyle.h>
@@ -25,7 +24,7 @@
 
 using namespace std;
 
-void make_graph(const vector<double>& years, const vector<double>& temps) {
+void make_graph_av(const vector<double>& years, const vector<double>& temps) {
     auto c = new TCanvas("c", "canvas for g");
     TGraph *g = new TGraph(years.size(), years, temps);
     g -> SetLineColor(4);
@@ -41,7 +40,27 @@ void make_graph(const vector<double>& years, const vector<double>& temps) {
     gStyle->SetPadGridY(true);
     gStyle->SetPadColor(0);
     g->Draw();
-    c->SaveAs("/out/graph.png");
+    c->SaveAs("/out/graph_av.png");
+}
+
+
+void make_graph_day(const vector<double>& years, const vector<double>& temps) {
+    auto c = new TCanvas("c", "canvas for g");
+    TGraph *g = new TGraph(years.size(), years, temps);
+    g -> SetLineColor(4);
+    g -> GetXaxis() -> SetTitle("Year");
+    g -> GetYaxis() -> SetTitle("Temperature");
+    gStyle->SetCanvasColor(0);
+    gStyle->SetPadTopMargin(0.15); 
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadLeftMargin(0.10); 
+    gStyle->SetPadRightMargin(0.10);
+    gStyle->SetFrameFillColor(0);
+    gStyle->SetPadGridX(true);     
+    gStyle->SetPadGridY(true);
+    gStyle->SetPadColor(0);
+    g->Draw();
+    c->SaveAs("/out/graph_Christmas.png");
 }
 
 void make_graph_min_max(const vector<double>& years, const vector<double>& minTemps, const vector<double>& maxTemps, const vector<double>& diffTemps) {
@@ -51,6 +70,8 @@ void make_graph_min_max(const vector<double>& years, const vector<double>& minTe
     TGraph *gr_diff = new TGraph (years.size(), years, diffTemps);
     gr_min -> SetTitle("The minimum and maximum temperature of the year");
     gr_min -> SetLineColor(4);
+    gr_max -> SetLineColor(2);
+    gr_diff -> SetLineColor(7);
     gr_min -> GetXaxis() -> SetTitle("Year");
     gr_min -> GetYaxis() -> SetTitle("Temperature");
     gStyle->SetCanvasColor(0);
