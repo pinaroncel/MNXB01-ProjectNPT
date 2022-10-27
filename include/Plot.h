e42943776666bc4244b77be693a5168684e2b4a8
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-27 12:36:42 +0200

    update Plot translation unit

--- include/Plot.h
+++ include/Plot.h
@@ -1,8 +1,8 @@
 
 // Plot.h by Tilde
 
-void make_graph_av(const vector<double>& years, const vector<double>& temps);
-
 void make_graph_day(const vector<double>& years, const vector<double>& temps);
+
+void make_graph_ave(const vector<double>& years, const vector<double>& aveTemps);
 
 void make_graph_min_max(const vector<double>& years, const vector<double>& minTemps, const vector<double>& maxTemps, const vector<double>& diffTemps);
--- src/Plot.cpp
+++ src/Plot.cpp
@@ -21,30 +21,10 @@
 
 using namespace std;
 
-void make_graph_av(const vector<double>& years, const vector<double>& temps) {
-    auto c = new TCanvas("c", "canvas for g");
-    TGraph *g = new TGraph(years.size(), years, temps);
-    g -> SetLineColor(4);
-    g -> SetTitle -> ("Average temperature");
-    g -> GetXaxis() -> SetTitle("Year");
-    g -> GetYaxis() -> SetTitle("Temperature");
-    gStyle->SetCanvasColor(0);
-    gStyle->SetPadTopMargin(0.15); 
-    gStyle->SetPadBottomMargin(0.15);
-    gStyle->SetPadLeftMargin(0.10); 
-    gStyle->SetPadRightMargin(0.10);
-    gStyle->SetFrameFillColor(0);
-    gStyle->SetPadGridX(true);     
-    gStyle->SetPadGridY(true);
-    gStyle->SetPadColor(0);
-    g->Draw();
-    c->SaveAs("/out/graph_av.png");
-}
-
-
 void make_graph_day(const vector<double>& years, const vector<double>& temps) {
     auto c = new TCanvas("c", "canvas for g");
-    TGraph *g = new TGraph(years.size(), years, temps);
+    // TGraph *g = new TGraph(years.size(), years, temps);
+    TGraph *g = new TGraph(years.size(), &(years[0]), &(temps[0]));
     g -> SetTitle("Temperature on Christmas");
     g -> SetLineColor(4);
     g -> GetXaxis() -> SetTitle("Year");
@@ -62,11 +42,35 @@ void make_graph_day(const vector<double>& years, const vector<double>& temps) {
     c->SaveAs("/out/graph_Christmas.png");
 }
 
+void make_graph_ave(const vector<double>& years, const vector<double>& aveTemps) {
+    auto c = new TCanvas("c", "canvas for g");
+    // TGraph *g = new TGraph(years.size(), years, temps);
+    TGraph *g = new TGraph(years.size(), &(years[0]), &(aveTemps[0]));
+    g -> SetLineColor(4);
+    g -> SetTitle -> ("Average temperature");
+    g -> GetXaxis() -> SetTitle("Year");
+    g -> GetYaxis() -> SetTitle("Temperature");
+    gStyle->SetCanvasColor(0);
+    gStyle->SetPadTopMargin(0.15); 
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadLeftMargin(0.10); 
+    gStyle->SetPadRightMargin(0.10);
+    gStyle->SetFrameFillColor(0);
+    gStyle->SetPadGridX(true);     
+    gStyle->SetPadGridY(true);
+    gStyle->SetPadColor(0);
+    g->Draw();
+    c->SaveAs("/out/graph_av.png");
+}
+
 void make_graph_min_max(const vector<double>& years, const vector<double>& minTemps, const vector<double>& maxTemps, const vector<double>& diffTemps) {
     auto c = new TCanvas("c", "canvas for g");
-    TGraph *gr_min = new TGraph (years.size(), years, minTemps);
-    TGraph *gr_max = new TGraph (years.size(), years, maxTemps);
-    TGraph *gr_diff = new TGraph (years.size(), years, diffTemps);
+    // TGraph *gr_min = new TGraph (years.size(), years, minTemps);
+    // TGraph *gr_max = new TGraph (years.size(), years, maxTemps);
+    // TGraph *gr_diff = new TGraph (years.size(), years, diffTemps);
+    TGraph *gr_min = new TGraph(years.size(), &(years[0]), &(minTemps[0]));
+    TGraph *gr_max = new TGraph(years.size(), &(years[0]), &(maxTemps[0]));
+    TGraph *gr_diff = new TGraph(years.size(), &(years[0]), &(diffTemps[0]));
     gr_min -> SetTitle("The minimum temperature of the year");
     gr_max -> SetTitle("The maximum temperature of the year");
     gr_diff -> SetTitle("The difference between the minimum and maximum temperatures");
