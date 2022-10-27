b922243c26c2539755d7d9b6009e1bc9de9ea3f1
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-27 20:21:32 +0200

    Update Read.h

--- include/Read.h
+++ include/Read.h
@@ -1,4 +1,6 @@
+
 // Read.h by Nikolei
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,5 +8,6 @@
 
 #include "Record.h"
 
+// -----------------------------------------------------------------------------
 // Reading the data file input and returns a vector of Record objects
 std::vector<Record> readFromFile(std::string filename);
