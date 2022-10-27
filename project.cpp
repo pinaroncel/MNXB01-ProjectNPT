98ff701e21fb442b7dc58d1daddb95bc5b5b51bc
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-27 13:33:39 +0200

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -12,12 +12,19 @@ using namespace std;
 
 int main() {
 
+    // STEP 0 : program start
     cout << " ... starting ProjectNPT ... " << endl;
 
-	string filename = "in/uppsala_tm_1722-2020.dat";
+	/* STEP 1 : READ by Nikolei
+     * read from file and store data in a vector of Record objects
+     */
+    string filename = "in/uppsala_tm_1722-2020.dat";
 
-    vector<Record> records   = readFromFile(filename);
+    vector<Record> records = readFromFile(filename);
 
+    /* STEP 2 : ANALYZE by Pinar
+     * analyze data and create vectors to be used in plotting
+    */
     vector<double> temps     = tempsOnDay(records, 12, 24);
     vector<double> years     = getAllYears(records);
     vector<double> aveTemps  = aveTempsPerYear(records);
@@ -33,10 +40,14 @@ int main() {
     // double* maxTempsArray  = convertVtoA(maxTemps);
     // double* diffTempsArray = convertVtoA(diffTemps);
 
+    /* STEP 3 : PLOT by Tilde
+     * plot and save graphs using the processed data
+    */
     make_graph_day(years, temps);
     make_graph_ave(years, aveTemps);
     make_graph_min_max_diff(years, minTemps, maxTemps, diffTemps);
 
+    // STEP 4 : program end
     cout << " ... ending ProjectNPT ... " << endl;
     return 0;
 }
