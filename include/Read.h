ee4cf48c8521645ee5b9ee5aead679fc03c45517
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-27 12:53:45 +0200

    updated Read translation unit

--- include/Read.h
+++ include/Read.h
@@ -1,4 +1,9 @@
 
 // Read.h by Nikolei
 
+#include <fstream>
+#include <string>
+#include <vector>
+
+// Returns a vector of Record objects storing the data from the input file
 vector<Record> readFromFile(string filename);
--- src/Read.cpp
+++ src/Read.cpp
@@ -2,7 +2,6 @@
 // Read.cpp by Nikolei
 
 #include <fstream>
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -11,19 +10,27 @@
 
 using namespace std;
 
+// Returns a vector of Record objects storing the data from the input file
 vector<Record> readFromFile(string filename) {
+
     vector<Record> records;
+
     ifstream input(filename);
     string line;
+
     while(getline(input, line)) {
+
         int    year      = stoi(line.substr(0, 4));
         int    month     = stoi(line.substr(5, 3));
         int    day       = stoi(line.substr(8, 3));
         double temp      = stod(line.substr(11, 6));
         double tempUrban = stod(line.substr(17, 6));
         int    dataId    = stoi(line.substr(23, 2));
+
         Record record = Record(year, month, day, temp, tempUrban, dataId);
+
         records.push_back(record);
     }
+
     return records;
 }
