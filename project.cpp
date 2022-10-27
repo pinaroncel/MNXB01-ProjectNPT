818d3c10884d0aba63e5b14f930d8b86f11f0094
Pinar Öncel <pinar.oncel@gmail.com>
2022-10-27 13:25:58 +0200

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -16,25 +16,24 @@ int main() {
 
     vector<Record> records   = readFromFile(filename);
 
-    vector<double> temps     = tempOnDay(records, 12, 24);
+    vector<double> temps     = tempsOnDay(records, 12, 24);
     vector<double> years     = getAllYears(records);
     vector<double> aveTemps  = aveTempsPerYear(records);
     vector<double> minTemps  = minTempsPerYear(records);
     vector<double> maxTemps  = maxTempsPerYear(records);
     vector<double> diffTemps = diffTempsPerYear(records);
+    vector<vector<double>> tempss = tempPerDay(records);
 
-    // vector<vector<double>> tempss = tempPerDay(records);
-
-    double* tempsArray     = convertVtoA(temps);
-    double* yearsArray     = convertVtoA(years);
-    double* aveTempsArray  = convertVtoA(aveTemps);
-    double* minTempsArray  = convertVtoA(minTemps);
-    double* maxTempsArray  = convertVtoA(maxTemps);
-    double* diffTempsArray = convertVtoA(diffTemps);
+    // double* tempsArray     = convertVtoA(temps);
+    // double* yearsArray     = convertVtoA(years);
+    // double* aveTempsArray  = convertVtoA(aveTemps);
+    // double* minTempsArray  = convertVtoA(minTemps);
+    // double* maxTempsArray  = convertVtoA(maxTemps);
+    // double* diffTempsArray = convertVtoA(diffTemps);
 
     make_graph_day(years, temps);
     make_graph_ave(years, aveTemps);
-    make_graph_min_max(years, minTemps, maxTemps, diffTemps);
+    make_graph_min_max_diff(years, minTemps, maxTemps, diffTemps);
 
     return 0;
 }
